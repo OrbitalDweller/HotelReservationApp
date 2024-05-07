98b671ece86e3671fa94340647a5b3b94e04e179
OrbitalDweller <dmitry.krasnikov@protonmail.com>
2024-05-07 19:25:40 -0400

    create room class

---
+++ src/model/Room.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Room implements IRoom {
+    private String roomNumber;
+    private Double price;
+    private RoomType roomType;
+
+    public Room(String roomNumber, Double price, RoomType roomType) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.roomType = roomType;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Double getRoomPrice() {
+        return price;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Room number " + roomNumber + " is $" + price + " per night and is " + roomType + " type";
+    }
+}
---
+++ src/model/Tester.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Tester {
+    public static void main(String[] args) {
+        Room room = new Room("123", 100.0, RoomType.DOUBLE);
+        System.out.println(room);
+    }
+}
